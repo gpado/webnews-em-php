5d6b4d5be1b5621a5e02ea14658aecc7a5991db3
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-10-19 14:14:56 -0300

    Fix: functions name and path to Connection class

--- src/php/posts/posts.php
+++ src/php/posts/posts.php
@@ -1,7 +1,7 @@
 <?php
 
     //Requiring Connection class
-    require_once("./connection.php");
+    require_once("../connection.php");
 
     class Posts{
 
@@ -64,19 +64,19 @@
 
         //CRUD functions
 
-        public function Submit(){
+        public function submit(){
             try{
                 $this->conn = new Connection();
-                $sql = $this->conn->prepare("INSERT INTO ".$this::TABLE_NAME." VALUES (null,?,?,?,?,?)");
-                @$sql->bindParams(1, $this->getTitle(), PDO::PARAM_STR);
-                @$sql->bindParams(2, $this->getCategory(), PDO::PARAM_STR);
-                @$sql->bindParams(3, $this->getPublicationDate(), PDO::PARAM_STR);
-                @$sql->bindParams(4, $this->getContent(), PDO::PARAM_STR);
-                @$sql->bindParams(5, $this->getImage(), PDO::PARAM_STR);
+                $sql = $this->conn->prepare("INSERT INTO posts VALUES (null,?,?,?,?,?)");    
+                @$sql->bindParam(1, $this->getTitle(), PDO::PARAM_STR);    
+                @$sql->bindParam(2, $this->getCategory(), PDO::PARAM_STR);    
+                @$sql->bindParam(3, $this->getPublicationDate(), PDO::PARAM_STR);    
+                @$sql->bindParam(4, $this->getContent(), PDO::PARAM_STR);    
+                @$sql->bindParam(5, $this->getImage(), PDO::PARAM_STR);    
 
-                if($sql->execute()){
+                if($sql->execute()){        
                     return true;
-                }else{
+                }else{        
                     return "Error unknow to submit a new post";
                 }
             }catch(PDO_Exception $error){
