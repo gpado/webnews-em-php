31a9a8704e9bf7787e1ce4f5beae819dfe8b4a1a
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-10-26 14:17:07 -0300

    Add: editPostData function to ControllerEdit class

--- src/php/posts/admin/controllerEdit.php
+++ src/php/posts/admin/controllerEdit.php
@@ -103,6 +103,38 @@
             <?php
             }
         }
+
+        public function editPostData(){
+            $id = $_POST['txbId'];
+            $title = $_POST['txbTitle'];
+            $category = $_POST['slctCategory'];
+            $content = $_POST['txaContent'];
+            $publicationDate = $_POST['txbPublicationDate'];
+            $image = $_POST['txbImage'];
+
+            $this->posts->setId($id);
+            $this->posts->setTitle($title);
+            $this->posts->setcategory($category);
+            $this->posts->setContent($content);
+            $this->posts->setPublicationDate($publicationDate);
+            $this->posts->setImage($image);
+
+            $serverResponse = $this->posts->edit();
+            if($serverResponse){
+                echo("
+                    <script>
+                        alert('Notícia editada com sucesso');
+                        document.location = 'listar_noticias.php';
+                    </script>
+                ");
+            }else{
+                echo("
+                    <h2>Oops, um erro ocorreu...</h2>
+                    <p>Tente recarregar a página ou acessar outra parte de nosso site pelo menu.</p>
+                    <script>console.log('Error to edit post: $serverResponse')</script>
+                ");
+            }
+        }
     }
 
     new ControllerEdit();
